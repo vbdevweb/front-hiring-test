dfd1aec45c2f3a0b2a6b4d5db7caae08a459e4d0
vbdevweb <vb.devweb@gmail.com>
2024-08-19 21:19:00 +0200

    Moved methods to API file and created a getTicketInformation function to get information about a single ticket

--- src/service/api.ts
+++ src/service/api.ts
@@ -2,18 +2,51 @@ import axiosLib from "axios";
 import { Ticket } from "../types";
 
 const axios = axiosLib.create({
-  baseURL: "https://kombo-nextjs-api.vercel.app/api",
-  timeout: 60000,
+	baseURL: "https://kombo-nextjs-api.vercel.app/api",
+	timeout: 60000,
 });
 
 export const Api = {
-  async getTickets() {
-    try {
-      const response = await axios.get<Ticket[]>("/tickets");
-      return response.data;
-    } catch (error) {
-      console.error("Error fetching tickets:", error);
-      throw error;
-    }
-  },
+	async getTickets() {
+		try {
+			const response = await axios.get<Ticket[]>("/tickets");
+			return response.data;
+		} catch (error) {
+			console.error("Error fetching tickets:", error);
+			throw error;
+		}
+	},
+	parseTime: (date: string) => {
+		const time = new Date(date).toLocaleTimeString("en-US", {
+			hour: "2-digit",
+			minute: "2-digit",
+		});
+		return time;
+	},
+	getTicketInformation(ticket: Ticket) {
+		const firstTrip = ticket.segments[0];
+		const lastTrip = ticket.segments[ticket.segments.length - 1];
+
+		return {
+			price: Math.round(ticket.price) + " €",
+			duration: firstTrip.duration,
+			transportType: firstTrip.transportType,
+			companySlugs: ticket.segments.map((segment) => segment.companySlug),
+			date: new Date(firstTrip.departure.time).toLocaleDateString("en-US", {
+				weekday: "short",
+				day: "numeric",
+				month: "short",
+			}),
+			departure: {
+				city: firstTrip.departure.city,
+				station: firstTrip.departure.station,
+				time: this.parseTime(firstTrip.departure.time),
+			},
+			arrival: {
+				city: lastTrip.arrival.city,
+				station: lastTrip.arrival.station,
+				time: this.parseTime(lastTrip.arrival.time),
+			},
+		};
+	},
 };
