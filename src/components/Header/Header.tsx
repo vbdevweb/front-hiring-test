57ca29a760ad35adb2731cc0e1a770a5ce40693c
vbdevweb <vb.devweb@gmail.com>
2024-08-19 21:07:05 +0200

    Updated header template and style

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,13 @@
-import React from "react";
 import { Link } from "react-router-dom";
-import { ReactComponent as Logo } from "./../../assets/svg/logo.svg";
-import "./Header.module.css";
+import Logo from "../../assets/svg/logo.svg";
+import "./Header.module.scss";
 
 export default function Header() {
-  return (
-    <header>
-      <Link to="/">
-        <Logo />
-      </Link>
-    </header>
-  );
+	return (
+		<header>
+			<Link to="/">
+				<Logo />
+			</Link>
+		</header>
+	);
 }
