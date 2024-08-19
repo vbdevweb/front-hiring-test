a69ce08f01a0300e4d893321c687a72c64237982
vbdevweb <vb.devweb@gmail.com>
2024-08-19 21:22:15 +0200

    Added SVG support to simplify import syntax

--- src/components/CompanyLogo.tsx
+++ src/components/CompanyLogo.tsx
@@ -1,24 +1,23 @@
-import React from "react";
 import { CompanySlug } from "../types";
-import { ReactComponent as AirfranceLogo } from "../assets/svg/companies/airfrance.svg";
-import { ReactComponent as FlixbusLogo } from "../assets/svg/companies/flixbus.svg";
-import { ReactComponent as InouiLogo } from "../assets/svg/companies/inoui.svg";
-import { ReactComponent as SncfLogo } from "../assets/svg/companies/sncf.svg";
+import AirfranceLogo from "../assets/svg/companies/airfrance.svg";
+import FlixbusLogo from "../assets/svg/companies/flixbus.svg";
+import InouiLogo from "../assets/svg/companies/inoui.svg";
+import SncfLogo from "../assets/svg/companies/sncf.svg";
 
 type Props = {
-  companySlug: CompanySlug;
+	companySlug: CompanySlug;
 };
 export function CompanyLogo({ companySlug }: Props) {
-  switch (companySlug) {
-    case "sncf":
-      return <SncfLogo />;
-    case "inoui":
-      return <InouiLogo />;
-    case "flixbus":
-      return <FlixbusLogo />;
-    case "airfrance":
-      return <AirfranceLogo />;
-    default:
-      return null;
-  }
+	switch (companySlug) {
+		case "sncf":
+			return <SncfLogo />;
+		case "inoui":
+			return <InouiLogo />;
+		case "flixbus":
+			return <FlixbusLogo />;
+		case "airfrance":
+			return <AirfranceLogo />;
+		default:
+			return null;
+	}
 }
