fa6deb79d4c8e519c96fe4d4d43b6221a2b5540d
vbdevweb <vb.devweb@gmail.com>
2024-08-19 21:27:20 +0200

    [TASK #2] Display ticket with dynamic data

--- src/pages/TicketInfo.tsx
+++ src/pages/TicketInfo.tsx
@@ -1,46 +1,60 @@
-import React from "react";
-import styles from "./TicketInfo.module.css";
-import { ReactComponent as FlightIcon } from "./../assets/svg/transportTypes/flight.svg";
-import { ReactComponent as AirFranceLogo } from "./../assets/svg/companies/airfrance.svg";
+import styles from "./TicketInfo.module.scss";
+import { useTicket } from "../components/TicketContext";
+import { Api } from "../service/api";
+import { CompanyLogo } from "../components/CompanyLogo";
+import { TransportIcon } from "../components/TransportIcon";
 
 export default function TicketInfo() {
-  // get this information from the ticket that the user selected.
-  const price = "485,00 €";
-  const date = "Fri, 27 Aug";
-  const departureTime = "7:00";
-  const departureCity = "Aéroport Barcelone-El Prat";
-  const departureStation = "Barcelona–El Prat Airport, BCN";
-  const arrivalTime = "13:30";
-  const arrivalCity = "Aéroport Lisbonne Humberto Delgado";
-  const arrivalStation = "Lisbon Portela Airport, LIS";
+	const { userTicket } = useTicket();
+	if (!userTicket) return;
 
-  return (
-    <div className={styles["ticket-info"]}>
-      <div className={styles["ticket"]}>
-        <div className={styles["date"]}>{date}</div>
-        {/* keep in mind, trip should be repeated for the number of trips on the ticket */}
-        <div className={styles["trip"]}>
-          <div className={styles["departure"]}>
-            <p className={styles["time"]}>{departureTime}</p>
-            <div className={styles["location"]}>
-              <p className={styles["city"]}>{departureCity}</p>
-              <p className={styles["station"]}>{departureStation}</p>
-            </div>
-          </div>
-          <div className={styles["transport-type"]}>
-            <FlightIcon />
-            <AirFranceLogo />
-          </div>
-          <div className={styles["arrival"]}>
-            <p className={styles["time"]}>{arrivalTime}</p>
-            <div className={styles["location"]}>
-              <p className={styles["city"]}>{arrivalCity}</p>
-              <p className={styles["station"]}>{arrivalStation}</p>
-            </div>
-          </div>
-        </div>
-        <div className={styles["price"]}>Total Price: {price}</div>
-      </div>
-    </div>
-  );
+	const ticketInfo = Api.getTicketInformation(userTicket!);
+
+	return (
+		<div className={styles["ticket-info"]}>
+			<div className={styles["ticket"]}>
+				<div className={styles["ticket--date"]}>{ticketInfo.date}</div>
+				{/* keep in mind, trip should be repeated for the number of trips on the ticket */}
+				<div className={styles["ticket--trip"]}>
+					<div className={styles["trip--departure"]}>
+						<p className={styles["departure--time"]}>{ticketInfo.departure.time}</p>
+						<div className={styles["departure--location"]}>
+							<p className={styles["location--city"]}>
+								{ticketInfo.departure.city}
+							</p>
+							<p className={styles["location--station"]}>
+								{ticketInfo.departure.station}
+							</p>
+						</div>
+					</div>
+					<div className={styles["trip--transport-type"]}>
+						<div className={styles["transport-type--icon"]}>
+							<TransportIcon
+								transportType={ticketInfo.transportType}
+							/>
+						</div>
+						<div className={styles["transport-type--companies"]}>
+							{ticketInfo.companySlugs.map((slug, i) => (
+								<div key={i} className={styles["company-logo"]}>
+									<CompanyLogo companySlug={slug} />
+								</div>
+							))}
+						</div>
+					</div>
+					<div className={styles["trip--arrival"]}>
+						<p className={styles["arrival--time"]}>{ticketInfo.arrival.time}</p>
+						<div className={styles["arrival--location"]}>
+							<p className={styles["location--city"]}>{ticketInfo.arrival.city}</p>
+							<p className={styles["location--station"]}>
+								{ticketInfo.arrival.station}
+							</p>
+						</div>
+					</div>
+				</div>
+				<div className={styles["ticket--price"]}>
+					Total Price: {ticketInfo.price}
+				</div>
+			</div>
+		</div>
+	);
 }
